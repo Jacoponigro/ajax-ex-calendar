e1fe776d41bd55184a41967b114a1b2a90df65e1
Jacopo <jakonigro@gmail.com>
2020-09-18 16:48:24 +0200

    layout e template

---
+++ script.js
@@ -0,0 +1,12 @@
+// document.ready
+
+$(document).ready(function(){
+// dichiaro variabile e uso template
+var data = "2018-01-01"
+var source = document.getElementById("entry-template").innerHTML;
+var template = Handlebars.compile(source);
+
+
+
+
+});
